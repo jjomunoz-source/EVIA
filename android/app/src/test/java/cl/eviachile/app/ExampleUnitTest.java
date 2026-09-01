1005a89e9217abcb01f5904a5b5386e718ac81aa
jjomunoz-source <jjomunoz@gmail.com>
2026-09-01 00:40:36 -0400

    Alinea paquete del test unitario Android

---
+++ android/app/src/test/java/cl/eviachile/app/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package cl.eviachile.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Local unit test for the EVIA Android project.
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
